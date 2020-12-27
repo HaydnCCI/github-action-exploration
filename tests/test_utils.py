657c37f9123f0863542c5f9376fbc41b51df6e5c
Haydn Cheong <haydncheong@Haydns-MacBook-Pro.local>
2020-12-27 18:47:48 +0000

    First commmit

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+from awesomecode.utils import add, remove_spaces
+import pytest
+
+@pytest.mark.parametrize('x, y, result', [
+    (10, 10, 20),
+    (5, 5, 10),
+    (6, 6, 12)
+])
+def test_add(x, y, result):
+    assert add(x, y) == result
+
+@pytest.mark.parametrize('data, result', [
+    ('Haydn C' , 'HaydnC'),
+    ('Tom   Peter', 'TomPeter')
+])
+def test_remove_spaces(data, result):
+    assert remove_spaces(data) == result
